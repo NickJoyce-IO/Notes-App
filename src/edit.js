82c31dc67f51e87631d432f6e65e80d6d5e691a6
Nick Joyce <brawlyrox@gmail.com>
2018-10-16 08:18:28 -0400

    first commit

---
+++ src/edit.js
@@ -0,0 +1,35 @@
+import { initializeEditPage, generateLastEdited } from './views'
+import { updateNote, removeNote } from './notes'
+
+const titleElement = document.querySelector('#note-title')
+const bodyElement = document.querySelector('#note-body')
+const editedElement = document.querySelector('#note-edited')
+const noteId = location.hash.substring(1)
+
+initializeEditPage(noteId)
+
+titleElement.addEventListener('input', (e) => {
+ const note = updateNote(noteId, {
+        title: e.target.value
+    })
+    editedElement.textContent = generateLastEdited(note.updatedAt)
+})
+
+bodyElement.addEventListener('input', (e) => {
+    const note = updateNote(noteId, {
+        body: e.target.value
+    })
+    editedElement.textContent = generateLastEdited(note.updatedAt)
+})
+
+document.querySelector('#remove-note').addEventListener('click', (e) => {
+    removeNote(noteId)
+    location.assign('/index.html')
+})
+
+window.addEventListener('storage', (e) => {
+    if (e.key === 'notes') {
+        initializeEditPage(noteId)
+    
+    }
+})
